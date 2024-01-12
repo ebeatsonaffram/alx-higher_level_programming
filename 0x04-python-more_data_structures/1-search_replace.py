a9f532432a6e48b3702d1d8f8a8ebda354ed41cb
root <enyonambeatsonaffram@gmail.com>
2024-01-12 10:57:54 -0800

    Update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return ("None")
-    else:
-        return my_list[idx]
+def search_replace(my_list, search, replace):
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
