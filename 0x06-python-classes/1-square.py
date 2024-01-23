2056293aa2e0bf7a914b2faece0c2eb64bab3175
root <enyonambeatsonaffram@gmail.com>
2024-01-23 13:13:44 -0800

    Update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# 1-square.py by Enyonam Beatson-Affram
-
+"""This module is an empty class square"""
 
 
 class Square:
+    """Define a square"""
 
-    def __init__(self, size):
-        """Initializing this square class
-        Args: size - represnets the size of the square defined
-        """
-
-        self.__size = size
+    pass
